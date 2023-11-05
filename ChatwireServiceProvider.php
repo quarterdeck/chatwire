1efde5b968f387f61f90ba27c126e2d54978f434
Kris Northfield <kris@quarterdeck.co.uk>
2023-11-05 13:55:25 +0000

    init

---
+++ ChatwireServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Quarterdeck;
+
+use Illuminate\Support\ServiceProvider;
+
+class ChatwireServiceProvider extends ServiceProvider
+{
+	/**
+	* Bootstrap any package services.
+	*/
+	public function boot(): void
+	{
+		$this->publishes([
+			__DIR__.'/../config/chatwire.php' => config_path('chatwire.php'),
+		]);
+		$this->loadRoutesFrom(__DIR__.'/../routes/web.php');
+		$this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+		$this->loadViewsFrom(__DIR__.'/../resources/views', 'chatwire');
+	}
+
+	public function register()
+	{
+		//
+	}
+}
---
+++ routes/web.php
@@ -0,0 +1,5 @@
+<?php
+
+Route::get('/chatwire', function () {
+	return view('chatwire::dashboard');
+});
