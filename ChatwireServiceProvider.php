5558c7a48c4ad2323cd6b6ba4639b7be762a84de
Kris Northfield <kris@quarterdeck.co.uk>
2023-11-05 16:36:09 +0000

    wip

--- ChatwireServiceProvider.php
+++ ChatwireServiceProvider.php
@@ -11,12 +11,13 @@ class ChatwireServiceProvider extends ServiceProvider
 	*/
 	public function boot(): void
 	{
-		$this->publishes([
-			__DIR__.'/../config/chatwire.php' => config_path('chatwire.php'),
-		]);
-		$this->loadRoutesFrom(__DIR__.'/../routes/web.php');
-		$this->loadMigrationsFrom(__DIR__.'/../database/migrations');
-		$this->loadViewsFrom(__DIR__.'/../resources/views', 'chatwire');
+		// $this->publishes([
+		// 	__DIR__.'/../config/chatwire.php' => config_path('chatwire.php'),
+		// ]);
+		// $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
+		// $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+		// $this->loadViewsFrom(__DIR__.'/../resources/views', 'chatwire');
+		Livewire::component('chatwire', Chatwire::class);
 	}
 
 	public function register()
